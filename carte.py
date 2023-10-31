de6db66a7c098cadb97803d90782e8233ecef530
Tom SCHIPPKE <tom.schippke12@gmail.com>
2023-10-31 15:18:04 +0100

    erreur df

--- carte.py
+++ carte.py
@@ -3,4 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 
-df = pd.read_csv('significant-earyhquake-database.csv', sep=';', index_col='ID Earthquake')
+df = pd.read_csv('C:/Users/SCHIPPKE/hackaton/pe-hackathon/significant-earthquake-database.csv', sep=';' , index_col='ID Earthquake')
+
+print(df.head())
+
+
+
+##df = pd.read_csv('significant-earthquake-database.csv', sep=';', index_col='ID Earthquake', usecols=['Flag Tsunami'])
+
