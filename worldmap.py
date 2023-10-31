2841f89a733b783a85d0f705c2bbbcc253cacf42
Clara Pruneau <clara.pruneau@etu.minesparis.psl.eu>
2023-10-31 17:45:56 +0100

    commit 2

--- worldmap.py
+++ worldmap.py
@@ -5,15 +5,19 @@ import numpy as np
 
 worldmap = pygal.maps.world.World()
 
-codes = pd.read_csv('C:/Users/clara/Cours-info/Hackathon/pe-hackathon/codespays.csv',index_col='Country')
+codes = pd.read_csv("./codespays.csv", encoding="Latin1", sep=';',index_col='Country')
+test = pd.read_csv("./dataframe_test.csv",sep=';')
+#print(codes)
 
 def dictionnaire(dataframe,colonne):
     df = codes.merge(dataframe, left_on = 'Country',right_on = 'Country')
     df.set_index('code', inplace=True)
     df[colonne].to_dict()
 
-def carte(dataframe,colonne):
-    D = dictionnaire(dataframe,colonne)
-    worldmap.add(D)
-    worldmap.render()
+
+D = dictionnaire(test,'Magnitude')
+worldmap.add('Magnitude',D)
+worldmap.render()
+worldmap
+
 
